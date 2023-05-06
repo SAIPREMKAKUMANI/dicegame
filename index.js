651633297b3fde5b704580b50ebbf03773acce52
SAIPREMKAKUMANI <112579832+SAIPREMKAKUMANI@users.noreply.github.com>
2023-05-06 19:30:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var n1=Math.floor(Math.random()*6)+1;
 var n2=Math.floor(Math.random()*6)+1;
-var randomImage1="images/dice"+n1+".png";
-var randomImage2="images/dice"+n2+".png";
+var randomImage1="dice"+n1+".png";
+var randomImage2="dice"+n2+".png";
 document.querySelectorAll("img")[0].setAttribute("src",randomImage1);
 document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
 var dice=document.querySelector("h1");
